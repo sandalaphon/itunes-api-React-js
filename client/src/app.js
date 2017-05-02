617cc289e09f0bd9eeedd3c9046c055f314748d0
Richard Page-Croft <rpcroft@gmail.com>
2017-05-02 19:30:53 +0100

    set up start point

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import MusicContainer from './containers/MusicContainer.jsx';
+
+window.onload = function () {
+  ReactDOM.render(
+    // <MusicContainer />,
+   <h1>Top 20 UK Singles Music Container to go here</h1>,
+    document.getElementById('app')
+  );
+};
